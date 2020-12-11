64f9feba364dd58c7ac02c7487d448ac7aa990be
Miroslav Jackanin <miroslav.jackanin@akademiasovy.sk>
2020-12-11 23:35:53 +0100

    cleaned code

--- app/src/main/java/sk/it/android/myapplication_listitemsfromstorage/MainActivity.java
+++ app/src/main/java/sk/it/android/myapplication_listitemsfromstorage/MainActivity.java
@@ -44,16 +44,6 @@ public class MainActivity extends AppCompatActivity {
         } else {
             requestMyPermissions();
         }
-        
-        /*if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-            if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE)) {
-                ActivityCompat.requestPermissions(MainActivity.this, new String[] {Manifest.permission.READ_EXTERNAL_STORAGE}, MY_PERMISSION_REQUEST);
-            } else {
-                ActivityCompat.requestPermissions(MainActivity.this, new String[] {Manifest.permission.READ_EXTERNAL_STORAGE}, MY_PERMISSION_REQUEST);
-            }
-        } else {
-            doStuff();
-        }*/
     }
 
     private void requestMyPermissions() {
